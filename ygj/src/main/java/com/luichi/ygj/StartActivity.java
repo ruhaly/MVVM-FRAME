b6ebe7310028e15f7807a0ac17e951753b3df2ac
ruhaly <hlr36hlr>
2016-09-13 19:07:46 +0800

    mvvm

--- ygj/src/main/java/com/luichi/ygj/StartActivity.java
+++ ygj/src/main/java/com/luichi/ygj/StartActivity.java
@@ -4,6 +4,7 @@ import android.view.View;
 
 import com.luichi.ygj.databinding.ActivityMainBinding;
 import com.luichi.ygj.ui.base.BaseActivity;
+import com.luichi.ygj.utils.L;
 import com.luichi.ygj.utils.Utils;
 
 public class StartActivity extends BaseActivity<ActivityMainBinding> {
@@ -16,9 +17,11 @@ public class StartActivity extends BaseActivity<ActivityMainBinding> {
     @Override
     public void initView() {
         dataBinding.button.setText("你好");
+
     }
 
     public void dClick(View view) {
+        L.e("马天骄");
         Utils.snack(this, "asdfasdf");
     }
 
